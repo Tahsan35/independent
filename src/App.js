dd2f1e38306f9454a56eb8eb6dba8f2503992c4f
Tahsan35 <tahsanknmas14@gmail.com>
2022-06-30 17:12:23 +0600

    reset project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 
-import { Route, Routes } from 'react-router-dom';
-import About from './components/About/About';
-import Blogs from './components/Blogs/Blogs';
-import CheckOut from './components/CheckOut/CheckOut';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import NotFound from './components/NotFound/NotFound';
-import Services from './components/Services/Services';
-import Signup from './components/Signup/Signup';
+
 function App() {
   return (
     <div>
-      <Header></Header>
-        {/*  */} 
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/services' element={<Services></Services>}></Route>
-        <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element={<About></About>} ></Route>
-        <Route path='/login' element={<Login></Login>} ></Route>
-        <Route path='/signup' element={<Signup></Signup>} ></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
-      <Routes>
-      </Routes>
-      <Footer></Footer>
     </div>
   );
 }
